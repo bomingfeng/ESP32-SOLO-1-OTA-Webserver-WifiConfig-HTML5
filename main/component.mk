12eb173ae0d29d54540997973a5e91bc3d4e7e09
bomingfeng <fengzhenbang@126.com>
2022-07-07 15:22:52 +0800

    create

--- main/component.mk
+++ main/component.mk
@@ -2,4 +2,4 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-COMPONENT_EMBED_TXTFILES :=  ${PROJECT_PATH}/server_certs/certs.pem
+# COMPONENT_EMBED_TXTFILES :=  ${PROJECT_PATH}/server_certs/certs.pem
